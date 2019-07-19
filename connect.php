36db6fe523cd3ebf941021bb4a316825ef55e85d
peemp <peem-41@hotmail.com>
2019-07-19 13:37:12 +0700

    sss

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect('localhost','root','gerrardxlk1') or die(mysql_error());
+$con = mysqli_connect('localhost','root','') or die(mysql_error());
 mysqli_select_db($con,'e-shop');
 ?>
