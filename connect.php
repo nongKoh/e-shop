2bfb4542a0374c4300a1fccf7d20aa895690f581
peemp <peem-41@hotmail.com>
2019-07-22 11:37:05 +0700

    44

--- connect.php
+++ connect.php
@@ -2,9 +2,4 @@
 $con = mysqli_connect('localhost','root','') or die(mysql_error());
 mysqli_select_db($con,'e-shop');
 mysqli_query($con,'set names utf8');
-<<<<<<< HEAD
-date_default_timezone_set("Asia/Bangkok");
-=======
-
->>>>>>> 9ae409bc2d61592ceafd571400124dde67127867
 ?>
